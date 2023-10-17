90098c5cca12337ba18edcc5df81679707dbe2ac
ren <137475269+reenweb@users.noreply.github.com>
2023-10-17 13:21:31 +0900

    10/17업데이트
    완료

--- js/ja_portfolio.js
+++ js/ja_portfolio.js
@@ -1,7 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 //햄버거 열기
 let menubtn = document.querySelector('.inner > .mobilemenu');
 let nav = document.querySelector('header > .inner > .menu');
 menubtn.addEventListener('click', () => {
-  nav.classList.toggle('on');
+nav.classList.toggle('on');
 });
 console.log(nav)
